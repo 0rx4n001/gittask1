efe2bdf345159e64d6e7c018175e4a38994e3c4a
0rx4n001 <m0njer0.002@gmail.com>
2023-11-15 19:15:18 +0400

    task1-complte-js

--- Task-1-js/script.js
+++ Task-1-js/script.js
@@ -1,1 +1,10 @@
 // sabah olacaq
+let userName = 'John';
+
+function showMessage() {
+    userName = "orxan"
+    let message = ( 'Hello '+userName );
+    console.log(message)
+  }
+
+showMessage()
